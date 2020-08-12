1cccbeccdb44941919db7a645331c29fe26a4cda
Alexander <megag96@mail.ru>
2020-08-12 09:41:34 +0300

    Version 1.0

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setApplicationName("ScreenTestKit");
+    QApplication::setApplicationVersion("1.0.0");
+    QApplication::setOrganizationName("Axel_k");
+    QApplication::setOrganizationDomain("https://github.com/3dproger");
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
